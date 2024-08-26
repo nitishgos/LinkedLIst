bb0a3ec6f9fc14958077e58a406fdcdf6d21ebb8
Nitish Ghosh <137692440+nitishgos@users.noreply.github.com>
2024-08-26 18:18:31 +0530

    Update Linkedlist.java

--- Linkedlist.java
+++ Linkedlist.java
@@ -231,15 +231,67 @@ public class Linkedlist{
          prev.next=null;
 
     }
+    public Node getMid(Node head){
+        Node slow=head;
+        Node fast=head.next;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow ;//mid
+    }
+    public  Node merge(Node head1,Node head2){
+        Node mergeLL=new Node(-1);
+        Node temp=mergeLL;
+        while(head1!=null && head2!=null){
+            if(head1.data <= head2.data){
+                temp.next=head1;
+                head1=head1.next;
+                temp=temp.next;
+            }else{
+                temp.next=head2;
+                head2=head2.next;
+                temp=temp.next;
+            }
+        }
+            while(head1!=null){
+                temp.next=head1;
+                head1=head1.next;
+                temp=temp.next;
+            }
+            while(head2!=null){
+                temp.next=head2;
+                head2=head2.next;
+                temp=temp.next;
+            }
+        
+        return mergeLL.next;
+        }
+    public Node mergesort(Node head){
+        if(head==null || head.next==null){
+            return head;
+        }
+        //find mid
+          Node mid=getMid(head);
+
+        //ms for lf and rf
+        Node righthead=mid.next;
+        mid.next=null;
+        Node leftnode=mergesort(head);
+        Node rightnode=mergesort(righthead);
+        //merge
+        return merge(leftnode,rightnode);
+    }
     public static void main(String[] args) {
         Linkedlist li=new Linkedlist();
-         head=new Node(1);
-        Node temp=new Node(2);
-        head.next=temp;
-        head.next.next=new Node(3);
-        head.next.next.next=temp;
-        System.out.println(li.isCycle());
-        li.removeCycle();
-        System.out.println(li.isCycle());
+         li.addFirst(1);
+         li.addFirst(2);
+         li.addFirst(3);
+         li.addFirst(4);
+         li.addFirst(5);
+         li.addFirst(6);
+         li.printList();
+         li.head= li.mergesort(head);
+         li.printList();
     }
 }
